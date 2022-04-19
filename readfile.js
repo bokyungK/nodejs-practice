44e86c026ccd3d84805d38f79fc8adc7b33d0445
bokyungK <freebob2b@gmail.com>
2022-04-19 21:26:44 +0900

    use fs with promise

--- readfile.js
+++ readfile.js
@@ -1,8 +1,10 @@
-const fs = require('fs');
-fs.readFile('./readme.txt', (err, data) => {
-    if (err) {
+const fs = require('fs').promises;
+
+fs.readFile('./readme.txt')
+    .then ((data) => {
+        console.log(data);
+        console.log(data.toString());
+    })
+    .catch((err) => {
         throw err;
-    }
-    console.log(data);
-    console.log(data.toString());
-})
+    });
