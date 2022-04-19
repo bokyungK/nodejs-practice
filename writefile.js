bbdf733b0ed0731b84d5e46ba2efa18e671a3392
bokyungK <freebob2b@gmail.com>
2022-04-19 21:36:13 +0900

    use writefile and readfile with promise

--- writefile.js
+++ writefile.js
@@ -1,8 +1,12 @@
 const fs = require('fs').promises;
 
-fs.writeFile('./writeme.txt', '새롭게 파일과 글이 생성됩니다.')
+fs.writeFile('./writeme.txt',
+ '프로미스 체이닝으로 텍스트 파일을 생성하고 읽어옵니다.')
+    .then (() => {
+        return fs.readFile('./writeme.txt');
+    })
     .then ((data) => {
-
+        console.log(data.toString());
     })
     .catch((err) => {
         throw err;
